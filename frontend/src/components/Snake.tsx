e8f3824351c2fcbdee30db35904d542bf807dbd4
MentorPK <kowalski.pawel21@gmail.com>
2024-01-14 00:53:32 +0100

    ADDED snake segments generetion and basic retro style

--- frontend/src/components/Snake.tsx
+++ frontend/src/components/Snake.tsx
@@ -1,4 +1,4 @@
-import { Signal, signal, useSignal } from '@preact/signals';
+import { Signal, useSignal } from '@preact/signals';
 import generatePosition from '../functions/generatePosition';
 
 type Position = {
@@ -60,6 +60,59 @@ const getDirection = () => {
   return direction;
 };
 
+const SnakeSegmentStyle = ({ position }) => {
+  console.log(position, 'POSITION');
+  const x = position.x * 30;
+  const y = position.y * 30;
+  const style = {
+    left: `${x}px`,
+    bottom: `${y}px`,
+    background: '#326432',
+    width: '24px',
+    height: '24px',
+    border: '2px solid #253d25',
+    margin: '1px',
+    position: 'absolute',
+  };
+  return <div style={style}></div>;
+};
+const SnakeSegment = ({ position, direction }) => {
+  const segments = useSignal([]);
+  const arr = [...Array(3).keys()];
+  console.log(direction, 'DIRECTION');
+  const updateSegment = (x, y) => {
+    segments.value.push({ x, y });
+  };
+
+  const updatePosition = (offset, axis) => {
+    arr.forEach((_, idx) => {
+      const coord = position.value[axis] + offset * (idx + 1);
+      updateSegment(
+        axis === 'x' ? coord : position.value.x,
+        axis === 'y' ? coord : position.value.y
+      );
+    });
+  };
+
+  if (direction === 0) {
+    updatePosition(-1, 'y');
+  } else if (direction === 1) {
+    updatePosition(-1, 'x');
+  } else if (direction === 2) {
+    updatePosition(1, 'y');
+  } else {
+    updatePosition(1, 'x');
+  }
+
+  return (
+    <>
+      {segments.value.map((segment, idx) => (
+        <SnakeSegmentStyle position={segment} key={idx} />
+      ))}
+    </>
+  );
+};
+
 const Snake = () => {
   const snakeHead = useSignal({ x: 0, y: 0 });
   const snakeHeadX = generatePosition();
@@ -67,7 +120,13 @@ const Snake = () => {
   snakeHead.value = { x: snakeHeadX, y: snakeHeadY };
   //const direction = getDirection();
   const direction = generatePosition(3);
-  return <SnakeHeadStyle position={snakeHead} direction={direction} />;
+  console.log(snakeHead.value, 'SNAKEHEAD');
+  return (
+    <>
+      <SnakeHeadStyle position={snakeHead} direction={direction} />
+      <SnakeSegment position={snakeHead} direction={direction} />
+    </>
+  );
 };
 
 export default Snake;
